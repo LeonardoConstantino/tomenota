9b75a5f1d202205adc2aa750368b4c3950b0b379
LeonardoConstantino <76814929+LeonardoConstantino@users.noreply.github.com>
2021-07-09 19:52:10 -0300

    fazendo uma limpezinha

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-// const COMENTARIOS = document.querySelector('#comentario').value
-// const BTN_ADICIONAR = document.querySelector('#adicionar')
 const CONTAINER = document.querySelector('.container')
 const FORM = document.querySelector('form').elements
-// let arrObjNotas = JSON.parse(localStorage.getItem("arrObjNotas"))
-// let arrNotas
-    // if (arrObjNotas != null) {
-    //     arrNotas = arrObjNotas
-    // }
-    // CONTAINER.children[0].classList
-    // JSON.parse(localStorage.getItem("arrObjNotas"))[0].id
 
-// console.log(arrObjNotas?arrObjNotas:[])
+function atualiza_local_Storage(nota) {
+    localStorage.setItem(`${nota.id}`, JSON.stringify(nota))
+}
 
+function gerar_id_aleatorio() { 
+    return Math.floor(Math.random() * 50000)
+}
 
-exibe_notas()
+function reseta_inputs() {
+    FORM.titulo.value = ''
+    FORM.comentario.value = ''
+}
 
 FORM.adicionar.addEventListener('click', (e) => {
     e.preventDefault()
@@ -21,29 +20,15 @@ FORM.adicionar.addEventListener('click', (e) => {
     criar_nota(cria_obj_nota())
 })
 
-// console.log(FORM.cor)
-// console.log(cria_obj_nota())
-
-
 function exibe_notas() {
-    // Object.keys(localStorage).forEach(function(key){
-    //     console.log(localStorage.getItem(key))
-    //     criar_nota(localStorage.getItem(key))
-    // })
     CONTAINER.innerHTML = ''
     for (let i = 0; i < localStorage.length; i++) {
         criar_nota(
-            JSON.parse(localStorage.getItem(Object.keys(localStorage)[i]))
+            JSON.parse(
+                localStorage.getItem(Object.keys(localStorage)[i])
+            )
         )
     }
-}
-
-function atualiza_local_Storage(nota) {
-    localStorage.setItem(`${nota.id}`, JSON.stringify(nota))
-}
-
-function gerar_id_aleatorio() { 
-    return Math.floor(Math.random() * 5000)
 }
 
 function cria_obj_nota() {
@@ -59,12 +44,9 @@ function cria_obj_nota() {
         comentario: FORM.comentario.value,
         cor: corEscolhida
     }
-    console.log(nota)
     atualiza_local_Storage(nota)
     return nota
 }
-
-
 
 function criar_nota(objNota) {
     let divNota = document.createElement("div")
@@ -83,43 +65,12 @@ function criar_nota(objNota) {
     btnApagaNota.classList.add(`${objNota.id}`)
     btnApagaNota.addEventListener("click", (e) => {
         const CLASSE_DO_BOTAO_CLICADO = e.target.classList[1]
-        console.log(CLASSE_DO_BOTAO_CLICADO)
         localStorage.removeItem(`${CLASSE_DO_BOTAO_CLICADO}`)
         divNota.remove()
     })
     divNota.appendChild(btnApagaNota)
 
-    FORM.titulo.value = ''
-    FORM.comentario.value = ''
+    reseta_inputs()
 }
 
-
-
-
-
-
-// function add_event_listener_btn_apagar() {
-//     const BTN_APAGAR = document.querySelectorAll('.apagar')
-//     for (let i = 0; i < BTN_APAGAR.length; i++) {
-//         BTN_APAGAR[i].addEventListener("click", (e) => {
-//             const CLASSE_DO_BOTAO_CLICADO = e.target.classList[1]
-//             console.log(CLASSE_DO_BOTAO_CLICADO)
-//             // localStorage.removeItem(`${CLASSE_DO_BOTAO_CLICADO}`)
-//         })
-//     }
-    
-// }
-
-
-
-
-// BTN_CORES[i].addEventListener("click", (e) => {
-//     const CLASSE_DO_BOTAO_CLICADO = e.target.classList[1]
-
-
-
-/* <div class="nota" style="background-color: #FFD899">
-    <h3>Lorem, ipsum dolor</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum vitae nostrum deserunt rerum repellendus ipsam quisquam, minus iure obcaecati natus</p>
-    <input type="button" value="Apagar" class="apagar">
-</div> */
+exibe_notas()
