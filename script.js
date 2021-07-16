18039b6fca1bc871a312e2b944e13eb1a3957e94
LeonardoConstantino <76814929+LeonardoConstantino@users.noreply.github.com>
2021-07-16 16:59:38 -0300

    organizando um pouco

--- script.js
+++ script.js
@@ -4,17 +4,102 @@ const ICONE_PESQUISA = document.querySelector('#search')
 const FORM = document.querySelector('form').elements
 const AUDIO = document.querySelectorAll('audio')
 
+puxa_notas_localStorage()
+
+function puxa_notas_localStorage() {
+    let listaNotas = []
+    for (let i = 0; i < localStorage.length; i++) {
+        listaNotas.push(Object.keys(localStorage)[i])
+    }
+    listaNotas.sort()
+    for (let i = 0; i < listaNotas.length; i++) {
+        exibe_notas_na_tela(JSON.parse(localStorage.getItem(listaNotas[i])))
+    }
+}
+
+function cria_obj_nota() {
+    const NOTA = {
+        id: gerar_id_momento_atual(),
+        titulo: FORM.titulo.value,
+        comentario: FORM.comentario.value,
+        cor: cor_escolhida(FORM.cor.value)
+    }
+    atualiza_local_Storage(NOTA)
+    return NOTA
+}
+
+function gerar_id_momento_atual() { 
+    const DATA = new Date()
+    return DATA.getTime()
+}
+
+function cor_escolhida(cor) {
+    if (cor == 'branco') return 'white'
+    if (cor == 'amarelo') return 'var(--cor1)'
+    if (cor == 'verde') return 'var(--cor2)'
+    if (cor == 'vermelho') return 'var(--cor3)'
+    if (cor == 'azul') return 'var(--cor4)'
+}
+
+function atualiza_local_Storage(nota) {
+    localStorage.setItem(`${nota.id}`, JSON.stringify(nota))
+}
+
+function exibe_notas_na_tela(objNota) {
+    const DIVNOTA = document.createElement("div")
+    DIVNOTA.setAttribute('class', 'nota' )
+    DIVNOTA.classList.add('nota_hover' )
+    DIVNOTA.style.backgroundColor = `${objNota.cor}`
+    DIVNOTA.innerHTML = `
+    <h3>${objNota.titulo}</h3>
+    <p>${objNota.comentario}</p>
+    `
+    CONTAINER.appendChild(DIVNOTA)
+
+    const BTNAPAGANOTA = document.createElement("input")
+    BTNAPAGANOTA.setAttribute('type', 'button')
+    BTNAPAGANOTA.setAttribute('value', 'delete')
+    BTNAPAGANOTA.setAttribute('class', 'apagar')
+    BTNAPAGANOTA.classList.add(`${objNota.id}`)
+    BTNAPAGANOTA.classList.add('material-icons')
+
+    BTNAPAGANOTA.addEventListener("click", (e) => {
+        const CLASSE_DO_BOTAO_CLICADO = e.target.classList[1]
+        localStorage.removeItem(`${CLASSE_DO_BOTAO_CLICADO}`)
+        DIVNOTA.classList.remove('nota_hover')
+        DIVNOTA.classList.add('animaApagar')
+        tocar_audio(0)
+        setTimeout(() => {
+            DIVNOTA.remove()
+        }, 300);
+
+    })
+    DIVNOTA.appendChild(BTNAPAGANOTA)
+
+    reseta_inputs()
+}
+
+function tocar_audio(audio) {
+    AUDIO[audio].volume = 0.2
+    AUDIO[audio].play()
+}
+
+function reseta_inputs() {
+    FORM.titulo.value = ''
+    FORM.comentario.value = ''
+}
+
 function pesquisa_notas() {
     const PESQUISA = INPUT_PESQUISA.value
-    let notas = document.querySelectorAll('.nota')
-    for (let i = 0; i < notas.length; i++) {
-        notas[i].remove()
+    const NOTAS = document.querySelectorAll('.nota')
+    for (let i = 0; i < NOTAS.length; i++) {
+        NOTAS[i].remove()
     }
-    if (!PESQUISA) {exibe_notas()}
+    if (!PESQUISA) {puxa_notas_localStorage()}
     for (let i = 0; i < localStorage.length; i++) {
         nota = JSON.parse(localStorage.getItem(Object.keys(localStorage)[i]))
         if (nota.titulo == PESQUISA || nota.comentario == PESQUISA) {
-            criar_nota(nota)
+            exibe_notas_na_tela(nota)
         }
     }
 }
@@ -23,39 +108,13 @@ INPUT_PESQUISA.addEventListener('input', () => {
     pesquisa_notas()
 })
 
-function atualiza_local_Storage(nota) {
-    localStorage.setItem(`${nota.id}`, JSON.stringify(nota))
-}
-
-function gerar_id_momento_atual() { 
-    const d = new Date()
-    return d.getTime()
-}
-
-function reseta_inputs() {
-    FORM.titulo.value = ''
-    FORM.comentario.value = ''
-}
-
 FORM.adicionar.addEventListener('click', (e) => {
     e.preventDefault()
     if (FORM.titulo.value == '' ||FORM.comentario.value == '') return
-    AUDIO[1].volume = 0.2
-    AUDIO[1].play()
-    criar_nota(cria_obj_nota())
+    tocar_audio(1)
+    exibe_notas_na_tela(cria_obj_nota())
     FORM.titulo.focus()
 })
-
-function exibe_notas() {
-    let listaNotas = []
-    for (let i = 0; i < localStorage.length; i++) {
-        listaNotas.push(Object.keys(localStorage)[i])
-    }
-    listaNotas.sort()
-    for (let i = 0; i < listaNotas.length; i++) {
-        criar_nota(JSON.parse(localStorage.getItem(listaNotas[i])))
-    }
-}
 
 function criar_notas_test(qtd) {
     let cont = 1
@@ -68,62 +127,8 @@ function criar_notas_test(qtd) {
             comentario: cont*Math.floor(Math.random() * 1+100),
             cor: core[Math.floor(Math.random() * core.length)]
         }
-        criar_nota(nota)
+        exibe_notas_na_tela(nota)
         atualiza_local_Storage(nota)
         ++cont
     }, 50)
 }
-
-function cria_obj_nota() {
-    let corEscolhida
-    if (FORM.cor.value == 'branco') {corEscolhida = 'white'}
-    if (FORM.cor.value == 'amarelo') {corEscolhida = 'var(--cor1)'}
-    if (FORM.cor.value == 'verde') {corEscolhida = 'var(--cor2)'}
-    if (FORM.cor.value == 'vermelho') {corEscolhida = 'var(--cor3)'}
-    if (FORM.cor.value == 'azul') {corEscolhida = 'var(--cor4)'}
-    let nota = {
-        id: gerar_id_momento_atual(),
-        titulo: FORM.titulo.value,
-        comentario: FORM.comentario.value,
-        cor: corEscolhida
-    }
-    atualiza_local_Storage(nota)
-    return nota
-}
-
-function criar_nota(objNota) {
-    let divNota = document.createElement("div")
-    divNota.setAttribute('class', 'nota' )
-    divNota.classList.add('nota_hover' )
-    divNota.style.backgroundColor = `${objNota.cor}`
-    divNota.innerHTML = `
-    <h3>${objNota.titulo}</h3>
-    <p>${objNota.comentario}</p>
-    `
-    CONTAINER.appendChild(divNota)
-
-    let btnApagaNota = document.createElement("input")
-    btnApagaNota.setAttribute('type', 'button')
-    btnApagaNota.setAttribute('value', 'delete')
-    btnApagaNota.setAttribute('class', 'apagar')
-    btnApagaNota.classList.add(`${objNota.id}`)
-    btnApagaNota.classList.add('material-icons')
-
-    btnApagaNota.addEventListener("click", (e) => {
-        const CLASSE_DO_BOTAO_CLICADO = e.target.classList[1]
-        localStorage.removeItem(`${CLASSE_DO_BOTAO_CLICADO}`)
-        divNota.classList.remove('nota_hover')
-        divNota.classList.add('animaApagar')
-        AUDIO[0].volume = 0.2
-        AUDIO[0].play()
-        setTimeout(() => {
-            divNota.remove()
-        }, 300);
-
-    })
-    divNota.appendChild(btnApagaNota)
-
-    reseta_inputs()
-}
-
-exibe_notas()
